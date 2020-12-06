a55de4f179dff3999d8ddc4087154e577040ba72
Dinh-Hoang Nam <dinhhoangnam162793@gmail.com>
2020-12-06 08:36:39 +0700

    :white_check_mark: testing things ok!

--- routes/university/university-list-router.js
+++ routes/university/university-list-router.js
@@ -1,13 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const connection = require("../../db");
-const { DB_NAME } = require("../../constance");
 const COLL_NAME = "University";
 const { authen } = require("../user-mng/protect-middleware");
 
 router.get("/universities", authen, async (req, res) => {
   try {
-    const col = (await connection).db(DB_NAME).collection(COLL_NAME);
+    const col = (await connection).db().collection(COLL_NAME);
     const universities = await col.find({}).toArray();
     res.json(universities);
   } catch (error) {
